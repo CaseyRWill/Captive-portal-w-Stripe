749cef57f975f33a2fc812b66d82ee45bfa16d25
CaseyRWill <165740228+CaseyRWill@users.noreply.github.com>
2024-11-11 17:24:28 -0900

    Update secrets.php

--- htdocs/secrets.php
+++ htdocs/secrets.php
@@ -3,4 +3,4 @@
 // or in a private script that does not publicly expose the source code.
 
 // This is your test secret API key.
-$stripeSecretKey = 'sk_test_51QIdN6K1ucZlE5ZZT9KTTPbgTy3IeTnjRNCA4fSqMMtZVmDYEcYYREuOwQmx6pxiLVv39izaV28L4Dggahb2W8U700J1pYQYEn';
+$stripeSecretKey = '*****';
