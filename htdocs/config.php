5d4a64a11f3894bbb9bb0952bc426ade13dfc9dd
CaseyRWill <165740228+CaseyRWill@users.noreply.github.com>
2024-11-11 17:24:48 -0900

    Update config.php

--- htdocs/config.php
+++ htdocs/config.php
@@ -1,7 +1,7 @@
 <?php
 
 $controlleruser = 'DirectConnect';
-$controllerpassword = 'f0rTUn3 1s 8olD!';
+$controllerpassword = '********';
 $controllerurl = 'https://24.237.130.252:443';
 $controllerversion = '8.5.6';
 
